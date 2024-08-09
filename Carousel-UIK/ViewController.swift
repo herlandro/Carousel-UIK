fa483a1d11fa0a065ff42c7f41372e0f173e0ed9
Herlandro Hermogenes <herlandro.hermogenes@gmail.com>
2024-08-09 05:29:50 +0100

    Created Project

---
+++ Carousel-UIK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carousel-UIK
+//
+//  Created by Herlandro Hermogenes on 08/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
