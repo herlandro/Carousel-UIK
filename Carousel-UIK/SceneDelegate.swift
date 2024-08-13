4a88e930897c7ec98f9c3a46626ee2594593cbeb
Herlandro Hermogenes <herlandro.hermogenes@gmail.com>
2024-08-13 21:52:13 +0100

    Refactored SceneDelegate to set initial viewController with newer approach

--- Carousel-UIK/SceneDelegate.swift
+++ Carousel-UIK/SceneDelegate.swift
@@ -17,10 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = CarouselViewController()
-        window?.makeKeyAndVisible()
+        let window = UIWindow(windowScene: windowScene)
+        let initialViewController = CarouselViewController()
+        let navigationController = UINavigationController(rootViewController: initialViewController)
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
